d87690feec2cb2b574b73786643a7c3efff51581
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-02-09 17:36:28 +0545

    Integrate API on bike page

--- src/pages/bike/index.jsx
+++ src/pages/bike/index.jsx
@@ -1,27 +1,28 @@
 import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/use-fetch";
 import { Button } from "../../components/Button";
 
 export const Bike = () => {
   const { id } = useParams();
 
+  const { data, isLoading, error } = useFetch(`/bike/${id}`);
+
+  if (isLoading) return "Loading...";
+
+  if (error) return error || "Error Occured";
+
   return (
     <div className="max-w-300 h-[calc(100vh-350px)] mx-auto flex items-center gap-12.5">
       <img
         className="w-125 h-75 object-cover rounded-lg"
-        src={`/bike-${id}.jpg`}
+        src={`${import.meta.env.VITE_SERVER_URL}/${data?.bike.image}`}
         alt=""
       />
       <div>
-        <h2 className="text-3xl font-bold mb-4">Thunderbolt Rider</h2>
-        <p className="text-lg mb-6">
-          Unleash the power of lightning as you speed through the streets. Built
-          with a lightweight yet durable frame, it delivers smooth handling and
-          stable performance across city roads and highways. The ergonomic
-          seating position ensures comfort on long rides, while the responsive
-          braking system provides confidence and control in all conditions.
-        </p>
+        <h2 className="text-3xl font-bold mb-4">{data?.bike.name}</h2>
+        <p className="text-lg mb-6">{data?.bike.description}</p>
         <div className="flex gap-5 items-center">
-          <span className="font-bold text-xl">$ 90,000</span>
+          <span className="font-bold text-xl">Rs. {data?.bike.price}</span>
           <Button className="bg-green-600 hover:bg-green-700">
             View Offer
           </Button>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,19 @@
 import { Hero } from "./Hero";
 import { Card } from "../../components/Card";
 import { Link } from "react-router-dom";
-
-const images = [
-  { id: 1, src: "/bike-1.jpg" },
-  { id: 2, src: "/bike-2.jpg" },
-];
+import { useFetch } from "../../hooks/use-fetch";
 
 export const Home = () => {
+  const { data, isLoading, error } = useFetch("/bike");
+
+  if (isLoading) {
+    return "Loading...";
+  }
+
+  if (error) {
+    return error || "Error Occurred";
+  }
+
   return (
     <main className="w-300 mx-auto">
       <Hero />
@@ -16,9 +22,14 @@ export const Home = () => {
         <h2 className="text-3xl font-bold mb-10">Latest bikes</h2>
 
         <div className="grid grid-cols-3 gap-x-8 gap-y-12 mb-20">
-          {images.map((img) => (
-            <Link key={img.id} to={`bike/${img.id}`}>
-              <Card image={img} id={img.id} />
+          {data?.bikes.map((bike) => (
+            <Link key={bike._id} to={`bike/${bike._id}`}>
+              <Card
+                image={`${import.meta.env.VITE_SERVER_URL}/${bike.image}`}
+                desc={bike.description}
+                name={bike.name}
+                price={bike.price}
+              />
             </Link>
           ))}
         </div>
