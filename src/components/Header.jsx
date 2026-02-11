a5054d842ad5ec0976c799af6f3c8937e0f651dd
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-02-11 15:45:35 +0545

    Add dropdown menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Button } from "./Button";
-import { useFetch } from "../hooks/use-fetch";
+import { FiChevronDown, FiChevronUp, FiUser } from "react-icons/fi";
 import toast from "react-hot-toast";
 import { axios } from "../lib/axios";
+import { Button } from "./Button";
+import { useFetch } from "../hooks/use-fetch";
 
 export const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   const { data, refetch } = useFetch("/auth/me");
 
   const navigate = useNavigate();
@@ -34,8 +38,36 @@ export const Header = () => {
 
       <div className="text-lg">
         {data?.data ? (
-          <div onClick={handleLogout}>
-            <Button className="bg-green-600 hover:bg-green-700">Logout</Button>
+          <div className="relative flex justify-end text-right w-[167px]">
+            <div
+              onClick={() => setShowMenu(!showMenu)}
+              className="cursor-pointer max-w-min flex items-center gap-2 truncate"
+            >
+              {data?.data?.username}
+              {showMenu ? (
+                <FiChevronUp size={21} />
+              ) : (
+                <FiChevronDown size={21} />
+              )}
+            </div>
+
+            {showMenu && (
+              <div className="absolute top-8 right-0 p-1 bg-white rounded-xl border border-gray-100 shadow-md">
+                <div className="flex justify-center mt-2 mb-1">
+                  <div className="size-10 rounded-full flex justify-center items-center bg-black/20 text-black/60">
+                    <FiUser size={24} />
+                  </div>
+                </div>
+                <div className="block min-w-max text-left mb-3 px-3">
+                  {data?.data?.email}
+                </div>
+                <div onClick={handleLogout}>
+                  <Button className="bg-green-600 hover:bg-green-700 w-full">
+                    Logout
+                  </Button>
+                </div>
+              </div>
+            )}
           </div>
         ) : (
           <div className="flex items-center gap-7">
